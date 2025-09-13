59213f1a52c06e7818df103e3223666be167de3c
marc2k3 <marc2003@live.co.uk>
2025-09-13 09:22:40 +0100

    Update LibraryCallback.cpp

--- src/callbacks/LibraryCallback.cpp
+++ src/callbacks/LibraryCallback.cpp
@@ -10,20 +10,38 @@ namespace smp
 	public:
 		void on_items_added(metadb_handle_list_cref handles) noexcept final
 		{
-			EventDispatcher::Get().PutEventToAll(GenerateEvent_JsCallback(EventId::kFbLibraryItemsAdded, std::make_shared<metadb_handle_list>(handles)));
+			if (library_manager_v4::get()->is_initialized())
+			{
+				EventDispatcher::Get().PutEventToAll(GenerateEvent_JsCallback(EventId::kFbLibraryItemsAdded, std::make_shared<metadb_handle_list>(handles)));
+			}
 		}
 
 		void on_items_modified_v2(metadb_handle_list_cref handles, metadb_io_callback_v2_data&) noexcept final
 		{
-			EventDispatcher::Get().PutEventToAll(GenerateEvent_JsCallback(EventId::kFbLibraryItemsChanged, std::make_shared<metadb_handle_list>(handles), is_modified_from_hook()));
+			if (library_manager_v4::get()->is_initialized())
+			{
+				EventDispatcher::Get().PutEventToAll(GenerateEvent_JsCallback(EventId::kFbLibraryItemsChanged, std::make_shared<metadb_handle_list>(handles), is_modified_from_hook()));
+			}
 		}
 
 		void on_items_removed(metadb_handle_list_cref handles) noexcept final
 		{
-			EventDispatcher::Get().PutEventToAll(GenerateEvent_JsCallback(EventId::kFbLibraryItemsRemoved, std::make_shared<metadb_handle_list>(handles)));
+			if (library_manager_v4::get()->is_initialized())
+			{
+				EventDispatcher::Get().PutEventToAll(GenerateEvent_JsCallback(EventId::kFbLibraryItemsRemoved, std::make_shared<metadb_handle_list>(handles)));
+			}
 		}
 
-		void on_library_initialized() noexcept final {}
+		void on_library_initialized() noexcept final
+		{
+			fb2k::inMainThread([this]
+				{
+					metadb_handle_list items;
+					library_manager::get()->get_all_items(items);
+					on_items_added(items);
+				});
+		}
+
 		void on_items_modified(metadb_handle_list_cref) noexcept final {}
 	};
 
